68eb9cdd5916d3b8a8c7236a6778a85d623fc15a
Dasari Rajnarayana <drajnaryana@gmail.com>
2022-02-11 10:13:43 +0530

    page updatd

---
+++ src/components/Profile/Photo/Photo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import  "./photo.css"
+
+function Photo() {
+    return (
+      <div className="photobody">
+        <div className="photodiv">
+          <img src="/rajnarayana.png" />
+        </div>
+      </div>
+    );
+}
+
+export default Photo
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./profile.css";
+
+import Photo from "./Photo/Photo"
+
+function Profile() {
+    return (
+      <div className="profilebody" >
+        
+        <div>
+          <Photo />
+        </div>
+       
+      </div>
+    );
+}
+
+export default Profile
